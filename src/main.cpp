0f17bd854eef74a02a7c0ac10755e33a7fed6ad1
web3ten0 <web3ten0@gmail.com>
2022-08-08 00:28:51 +0900

    Escapeで閉じるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,31 @@
 #include <GLFW/glfw3.h>
 #endif
 
+#include <iostream>
+using namespace std;
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main(void) {
-  GLFWwindow *window;
-
-  /* Initialize the library */
-  if (!glfwInit())
-    return -1;
-
-  /* Create a windowed mode window and its OpenGL context */
+  GLFWwindow* window;
+  if (!glfwInit()) return -1;
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (!window) {
+    cout << "failed to create window." << endl;
     glfwTerminate();
     return -1;
   }
-
-  /* Make the window's context current */
+  cout << "opened window" << endl;
+  glfwSetKeyCallback(window, key_callback);
   glfwMakeContextCurrent(window);
-
-  /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
-    /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
-
-    /* Swap front and back buffers */
     glfwSwapBuffers(window);
-
-    /* Poll for and process events */
     glfwPollEvents();
   }
-
   glfwTerminate();
-  return 0;
+  cout << "closed window" << endl;
 }
