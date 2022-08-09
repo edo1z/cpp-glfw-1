75e6ca8d6324926d8f40bf3cac80da100a4eae5f
web3ten0 <web3ten0@gmail.com>
2022-08-09 18:18:08 +0900

    added GLEW

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #endif
 
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
@@ -13,7 +15,8 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main(void) {
   GLFWwindow* window;
-  if (!glfwInit()) return -1;
+  if (!glfwInit()) return 1;
+  atexit(glfwTerminate);
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -23,20 +26,25 @@ int main(void) {
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (!window) {
     cout << "failed to create window." << endl;
-    glfwTerminate();
-    return -1;
+    return 1;
   }
   cout << "opened window" << endl;
   glfwSetKeyCallback(window, key_callback);
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
 
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    cerr << "Can't initialize GLEW" << endl;
+    return 1;
+  }
+
   const GLubyte* renderer = glGetString(GL_RENDERER);
   const GLubyte* version = glGetString(GL_VERSION);
   std::cout << "Renderer: " << renderer << std::endl;
   std::cout << "OpenGL version supported: " << version << std::endl;
 
-  glClearColor(0.0f, 0.3f, 0.6f, 0.3f);
+  glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
 
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -44,6 +52,5 @@ int main(void) {
     glfwPollEvents();
   }
 
-  glfwTerminate();
   cout << "closed window" << endl;
 }
